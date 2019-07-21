b3ab6d28a35e489280736b02b7529672153dc045
Reaper <reaper-16.17@mail.ru>
2019-07-21 13:28:27 +0300

    #1 init commit. Added Binary Search simple impl

---
+++ src/main/java/algo/BinarySearch.java
@@ -0,0 +1,37 @@
+package algo;
+
+import com.sun.istack.internal.NotNull;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    private static int binarySearch(@NotNull Comparable arr[], @NotNull Comparable toFind ){
+        // check if unsorted!
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high){
+            int current = (low + high)/2;
+            if (arr[current].compareTo(toFind) == 0) return current;
+            if (arr[current].compareTo(toFind) < 0){
+                low = low + current;
+            }
+            else {
+                high = high - current;
+            }
+        }
+        return -1;
+    }
+
+
+
+    public static void main(String[] args) {
+        Integer[] nums = new Integer[10];
+        for(int i = 0; i < 10; i++){
+            nums[i] = i*2;
+        }
+        int found = binarySearch(nums, 4);
+        System.out.println("Given arr = " + Arrays.toString(nums));
+        System.out.println("found pos = " + found);
+    }
+}
---
+++ src/main/java/basics/Primitives.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class Primitives {
+    public static void main(String[] args) {
+
+//        Примитивы (byte, short, int, long, char, float, double, boolean)
+//        Циклы и ветвления (if, else, switch, case, default, while, do, break, continue, for)
+//        Исключения (try, catch, finally, throw, throws)
+//        Области видимости (private, protected, public)
+//        Объявление \ Импорт (import, package, class, interface, extends, implements, static, final, void, abstract, native)
+//        Создание \ Возврат \ Вызов (new, return, this, super)
+//        Многопоточность (synchronized, volatile)
+//        instanceof, enum, assert, transient, strictfp, const, goto
+
+    }
+}
